e81bcaec9d1d9342cc4f3d9b3770d0a47a98fe9f
giuseppe.corti <giuseppe.corti@lightstreamer.com>
2021-03-24 12:55:45 +0100

    Restyle package

--- src/main/java/com/lightstreamer/examples/chat_demo/adapters/ChatDataAdapter.java
+++ src/main/java/com/lightstreamer/examples/chat_demo/adapters/ChatDataAdapter.java
@@ -14,7 +14,7 @@
   limitations under the License.
 */
 
-package chat_demo.adapters;
+package com.lightstreamer.examples.chat_demo.adapters;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
