0b22d92f4b6893530c8aa91588f91893c07de81b
Dario Crivelli <dario.crivelli@lightstreamer.com>
2023-06-06 10:32:30 +0200

    Align to interface tweak.

--- src/main/java/com/lightstreamer/examples/chat_demo/adapters/ChatMetadataAdapter.java
+++ src/main/java/com/lightstreamer/examples/chat_demo/adapters/ChatMetadataAdapter.java
@@ -19,7 +19,6 @@ package com.lightstreamer.examples.chat_demo.adapters;
 import java.io.File;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.logging.log4j.LogManager;
@@ -96,7 +95,7 @@ public class ChatMetadataAdapter extends LiteralBasedProvider {
      * Triggered by a client "sendMessage" call.
      * The message encodes a chat message from the client.
      */
-    public CompletionStage<String> notifyUserMessage(String user, String session, String message)
+    public CompletableFuture<String> notifyUserMessage(String user, String session, String message)
         throws NotificationException, CreditsException {
 
         // we won't introduce blocking operations, hence we can proceed inline
